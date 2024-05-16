8e4b423d8bbb0f0b520a2f11b7735c7f63a3b7c9
nazhG <nazhirgonzalez@gmail.com>
2024-05-15 22:26:23 -0500

    base

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import './App.css'
+import dayjs from 'dayjs';
 
 function App() {
+  // Obtener la fecha actual
+  const currentDate = new Date();
+
+  // Array para almacenar los números de los días del mes (del 1 al 30)
+  const daysOfMonth = Array.from({ length: 30 }, (_, i) => i + 1);
 
   return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <main className="grid place-content-center my-auto h-full">
+      <div className="grid grid-cols-7 gap-4">
+        {daysOfMonth.map(day => (
+          <div key={day} className="flex items-center justify-center w-12 h-12 border border-gray-200 rounded-full">
+            {day}
+          </div>
+        ))}
+      </div>
+    </main>
   )
 }
 
